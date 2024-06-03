6be73698b1886148bcbd9c4a4eb7b60d62920f71
mohamed ichoubane <mohamedichoubanetata@gmail.com>
2024-06-03 16:52:03 +0100

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,32 @@
+<script setup>
+import Footer from './components/Footer.vue';
+
+</script>
+
 <template>
-  <h1>Hello from Vue3</h1>
+  <v-app>
+    <v-main>
+    <h1>Hello from Vue3</h1>
+    </v-main>
+    <Footer />
+  </v-app>
+
 </template>
+
+<!-- <style>
+html, body, #app, .v-application {
+  height: 100%;
+  margin: 0;
+}
+.v-application--wrap {
+  min-height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.v-main {
+  flex: 1 0 auto;
+}
+.v-footer {
+  flex-shrink: 0;
+}
+</style> -->
